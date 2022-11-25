a9aa519e5aceed3c31d36222ddea11ced1dbc2e8
rubenyw <rubenyasonwinarta@gmail.com>
2022-11-26 03:52:21 +0700

    Create youtube_mur.m

---
+++ youtube_mur.m
@@ -0,0 +1,19 @@
+clc; clear; close all; warning off all;
+
+obj = imread('banana.png');
+imshow(obj);
+
+b = rgb2gray(obj);
+figure, imshow(b);
+
+c = imbinarize(b);
+figure, imshow(c);
+
+d = imcomplement(c);
+figure, imshow(d);
+
+e = imfill(d, 'holes');
+figure, imshow(e);
+
+[labeled, numObjects] = bwlabel(e);
+figure, imshow(labeled);
