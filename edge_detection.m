d6f56273577639d0f00d1ee724cac68741e05af6
rubenyw <rubenyasonwinarta@gmail.com>
2022-11-30 16:54:52 +0700

    Update edge_detection.m

--- edge_detection.m
+++ edge_detection.m
@@ -1,12 +1,15 @@
 obj = imread('pisang 1.png');
 figure, imshow(obj);
 
+obj = imadjust(obj,[0.3 0.7],[]);
+figure, imshow(obj);
+
 obj = rgb2gray(obj);
 figure, imshow(obj);
 
-[~,threshold] = edge(obj,'log');
+[~,threshold] = edge(obj,'Log');
 fudgeFactor = 0.7;
-BWs = edge(obj,'log',threshold * fudgeFactor);
+BWs = edge(obj,'Log',threshold * fudgeFactor);
 figure, imshow(BWs);
 
 se90 = strel('line',3,90);
@@ -15,9 +18,11 @@ BWsdil = imdilate(BWs,[se90 se0]);
 figure, imshow(BWsdil);
 
 bwfill = imfill(BWsdil, 'holes');
+bwfill = bwareaopen(bwfill,200);
+bwfill = bwlabel(bwfill);
 figure, imshow(bwfill);
 
-seD = strel('diamond',6);
+seD = strel('diamond',4);
 BWfinal = imerode(bwfill,seD);
 BWfinal = imerode(BWfinal,seD);
 figure, imshow(BWfinal);
